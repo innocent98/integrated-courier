92c84f5ef93f3308d5d872a8c9968d056f37478e
Victor Oluwatosin <tosinadebayo55@gmail.com>
2024-07-11 14:16:52 +0000

    updated

--- src/app/api/order_status/route.ts
+++ src/app/api/order_status/route.ts
@@ -1,29 +1,29 @@
-// import { NextResponse } from "next/server";
-// import connectDB from "@/lib/db";
-// import Order from "@/models/Order";
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/db";
+import Order from "@/models/Order";
 
-// export const GET = async (request: any, { params }: any) => {
-//   await connectDB();
+export const GET = async (request: any) => {
+  await connectDB();
 
-//   const { id } = params;
+  // const { id } = params;
 
-//   try {
-//     const body = await request.json();
-//     const { trackingNo } = body;
+  try {
+    const body = await request.json();
+    const { trackingNo } = body;
 
-//     const order = await Order.findOne({ trackingNo: trackingNo });
+    const order = await Order.findOne({ trackingNo: trackingNo });
 
-//     if (order) {
-//       return new NextResponse(JSON.stringify(order), { status: 200 });
-//     } else {
-//       return new NextResponse(JSON.stringify({ message: "No order found!" }), {
-//         status: 404,
-//       });
-//     }
-//   } catch (err) {
-//     console.log("err", err);
-//     return new NextResponse(JSON.stringify({ error: "Connection Error" }), {
-//       status: 500,
-//     });
-//   }
-// };
+    if (order) {
+      return new NextResponse(JSON.stringify(order), { status: 200 });
+    } else {
+      return new NextResponse(JSON.stringify({ message: "No order found!" }), {
+        status: 404,
+      });
+    }
+  } catch (err) {
+    console.log("err", err);
+    return new NextResponse(JSON.stringify({ error: "Connection Error" }), {
+      status: 500,
+    });
+  }
+};
